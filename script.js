08b9d7132378b640859695e6ae34510c122d4882
Willowbi <sergioandresbadel16@gmail.com>
2024-10-01 23:07:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const music = document.getElementById('background-music');
+    music.volume = 0.5; // Ajusta el volumen si es necesario
+});
