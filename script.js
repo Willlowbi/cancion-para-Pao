5c7e364933a0bb2f84e6966563591db4f0e10710
Willowbi <sergioandresbadel16@gmail.com>
2024-10-02 00:31:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     const music = document.getElementById('background-music');
-    music.volume = 0.2; // Ajusta el volumen si es necesario
+    music.volume = 0.1; // Ajusta el volumen si es necesario
 });
